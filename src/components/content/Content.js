13d8c98034f959abb3cfe4493ad8c67421a86665
James Kerivan <jameskerivan@gmail.com>
2021-02-15 01:14:43 -0800

    fix

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,5 +1,11 @@
 
-
+import { useState, useEffect } from "react";
+import Terminal from '../terminal/Terminal.js';
+import About from './about/About.js'
+import News from './news/News.js'
+import Pool from './pool/Pool.js'
+import Home from './Home.js'
+import {Router, Switch, Route, useLocation} from 'react-router-dom';
 
 
 const Content = ()=> {
@@ -12,7 +18,16 @@ const Content = ()=> {
    }, []);
 
   return (
-
+    <div className="content-container">
+        <div className="content">
+            <Switch>
+                <Route exact path={["/", ""]} component={Home} />
+                <Route exact path={["/about"]} component={About} />
+                <Route exact path={["/news"]} component={News} />
+                <Route exact path={["/pool"]} component={Pool} />
+            </Switch>
+        </div>
+    </div>
   );
 }
 
